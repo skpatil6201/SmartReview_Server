9ddbf07eef56aeb7ad3bb26d4ff9bd83d9d960e0
Tejas Patil <tejaspatil77777@gmail.com>
2026-08-11 11:42:40 +0530

    update normal express apllication

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.get("/", (_req, res) => {
+  res.send("Hello from Express on port 8000!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
